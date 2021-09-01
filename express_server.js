12ac1f903743a8d9075c7819ce34075aa3b3ed25
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-09-01 02:39:59 -0700

    Added function unique id with ut decoding and checking unique key and value in urlDatabase

--- express_server.js
+++ express_server.js
@@ -4,10 +4,41 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+//app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+/*
+61 == count[0-9] + count[A-Z] + count[a-z]
+decimal 48 == '0' in utf-8
+decimal 65 == 'A' in utf-8
+decimal 97 == 'a' in utf-8
+*/
+const generateRandomString = (len = 6) => {
+  let randomStr = '';
+
+  for (let i = 0; i < len; i++) {
+    let charNum = Math.floor(Math.random() * 62) + 48;
+    if (charNum > 57) { //48+9
+      charNum += 7;
+    }
+    if (charNum > 90) { //48+7+35
+      charNum += 6;
+    }
+    randomStr += decodeURI(`%${charNum.toString(16)}`); //decimal to hexadecial then decoding utf-8
+  }
+
+  if (urlDatabase[randomStr]) {
+    randomStr = generateRandomString(len);
+  }
+
+  return randomStr;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -16,6 +47,19 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls : urlDatabase };
   res.render("urls_index", templateVars)
+});
+
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  if (Object.values(urlDatabase).indexOf(longURL) < 0) { //if value already exists
+    const id = generateRandomString(6);
+    urlDatabase[id] = longURL;
+  }
+  res.redirect("/urls");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
