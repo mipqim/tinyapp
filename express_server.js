47448a454abe96c718a764c4d8b36d777535e96b
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-08-31 22:29:16 -0700

    Added template files

--- express_server.js
+++ express_server.js
@@ -13,22 +13,28 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls : urlDatabase };
+  res.render("urls_index", templateVars)
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting : 'Hello World!' };
+  res.render('hello_world', templateVars);
 });
 
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
+});
  
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
