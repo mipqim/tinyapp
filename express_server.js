50188389bf7500e9837d013dddc9c68e27c0fec1
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-09-01 23:42:17 -0700

    Cookies in Express

--- express_server.js
+++ express_server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080; 
 
-app.set("view engine", "ejs");
-
 const bodyParser = require("body-parser");
-//app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+//app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,7 +16,7 @@ const urlDatabase = {
 };
 
 /*
-61 == count[0-9] + count[A-Z] + count[a-z]
+62 == count[0-9] + count[A-Z] + count[a-z]
 decimal 48 == '0' in utf-8
 decimal 65 == 'A' in utf-8
 decimal 97 == 'a' in utf-8
@@ -40,13 +42,9 @@ const generateRandomString = (len = 6) => {
   return randomStr;
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 //Read-index
 app.get("/urls", (req, res) => {
-  const templateVars = { urls : urlDatabase };
+  const templateVars = {username: req.cookies["username"], urls : urlDatabase};
   res.render("urls_index", templateVars)
 });
 
@@ -65,7 +63,8 @@ app.post("/urls", (req, res) => {
 
 //Page of creation
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 //Delete
@@ -94,7 +93,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const id = req.params.shortURL;
 
   if (urlDatabase[id]){
-    const templateVars = { shortURL : id, longURL : urlDatabase[id]};
+    const templateVars = { username: req.cookies["username"], shortURL : id, longURL : urlDatabase[id]};
     res.render("urls_show", templateVars)
   } else {
     res.redirect("/urls");
@@ -110,8 +109,25 @@ app.get("/u/:shortURL", (req, res) => {
   }
 });
 
+//Login
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username); 
+  res.redirect("/urls");  
+});
+
+//Logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");  
+});
+
 
 //Should delete? -start
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
